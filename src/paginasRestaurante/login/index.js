b5e9c7d595e6e2812bef4a77a69df82b66cd3948
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-28 13:54:06 -0300

    useState mostrar senha

--- src/paginasRestaurante/login/index.js
+++ src/paginasRestaurante/login/index.js
@@ -2,9 +2,11 @@ import './styles.css';
 import '../../styles/global.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export default function Login() {
+  const [mostrarSenha, setMostrarSenha] = useState(false);
   return (
     <div className="formLogin">
       <div className="flexRow mt2rem ml2rem selfStart">
@@ -17,8 +19,13 @@ export default function Login() {
         </div>
         <div className="flexColunm mb1rem inputPassword">
           <label htmlFor="senha">Senha</label>
-          <input id="senha" type="password" />
-          <FontAwesomeIcon className="eyePassword" icon={faEye} />
+          <input id="senha" type={mostrarSenha ? 'text' : 'password'} />
+          <FontAwesomeIcon
+            onClick={() => setMostrarSenha(!mostrarSenha)}
+            className="eyePassword"
+            icon={mostrarSenha ? faEye : faEyeSlash}
+            size="lg"
+          />
         </div>
         <div className="flexRow contentCenter mt1rem mb1rem">
           <button className="btLaranja" type="button"> Entrar </button>
