27a52ef33b89201b6d15b8a3ddcddef261d3dfd2
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-29 12:44:05 -0300

    inserindo react-hook-form no login

--- src/paginasRestaurante/login/index.js
+++ src/paginasRestaurante/login/index.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-console */
 import './styles.css';
 import '../../styles/global.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
 import illustrationCenter from '../../assets/illustration-center.svg';
 import InputPassword from '../../componentes/inputPassword';
 
 export default function Login() {
+  const { register, handleSubmit } = useForm('');
   const [password, setPassword] = useState('');
+
+  function onSubmit(data) {
+    console.log(data);
+  }
   return (
     <div className="bodyLogin">
       <div className="conteinerFormLogin">
@@ -15,17 +22,20 @@ export default function Login() {
           <div className="flexRow mt2rem ml2rem selfStart">
             <h1>Login</h1>
           </div>
+
           <div className=" flexColunm mb1rem mt2rem">
-            <div className="flexColunm mb1rem">
-              <label htmlFor="email">Email</label>
-              <input id="email" type="text" />
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flexColunm mb1rem">
+                <label htmlFor="email">Email</label>
+                <input id="email" type="text" {... register('email')} />
+              </div>
 
-            <InputPassword label="Senha" value={password} setValue={setPassword} />
+              <InputPassword id="senha" label="Senha" register={register} value={password} setValue={setPassword} />
 
-            <div className="flexRow contentCenter mt1rem mb1rem">
-              <button className="btLaranja" type="button" onClick={() => toast.error('1 2 3 testando!')}> Entrar </button>
-            </div>
+              <div className="flexRow contentCenter mt1rem mb1rem">
+                <button className="btLaranja" type="submit" onClick={() => toast.error('1 2 3 testando!')}> Entrar </button>
+              </div>
+            </form>
             <div className="flexRow contentCenter mt2rem">
               <span className="mr06rem">Ainda não tem uma conta?</span>
               <Link to="/cadastrar">Cadastre-se</Link>
