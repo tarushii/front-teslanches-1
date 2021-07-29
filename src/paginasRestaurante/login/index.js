4f007b464c7f40c6040de6806d544c8163e2683a
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-28 21:51:04 -0300

    componente senha implementado em login

--- src/paginasRestaurante/login/index.js
+++ src/paginasRestaurante/login/index.js
@@ -1,13 +1,12 @@
 import './styles.css';
 import '../../styles/global.css';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import illustrationCenter from '../../assets/illustration-center.svg';
+import InputPassword from '../../componentes/inputPassword';
 
 export default function Login() {
-  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [password, setPassword] = useState('');
   return (
     <div className="bodyLogin">
       <div className="conteinerFormLogin">
@@ -20,16 +19,9 @@ export default function Login() {
               <label htmlFor="email">Email</label>
               <input id="email" type="text" />
             </div>
-            <div className="flexColunm mb1rem inputPassword">
-              <label htmlFor="senha">Senha</label>
-              <input id="senha" type={mostrarSenha ? 'text' : 'password'} />
-              <FontAwesomeIcon
-                onClick={() => setMostrarSenha(!mostrarSenha)}
-                className="eyePassword"
-                icon={mostrarSenha ? faEye : faEyeSlash}
-                size="lg"
-              />
-            </div>
+
+            <InputPassword label="Senha" value={password} setValue={setPassword} />
+
             <div className="flexRow contentCenter mt1rem mb1rem">
               <button className="btLaranja" type="button"> Entrar </button>
             </div>
