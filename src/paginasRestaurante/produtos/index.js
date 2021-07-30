ec069de05256f0a411fda7c547db89ed0861aba8
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-30 17:17:21 -0300

    pag produtos

--- src/paginasRestaurante/produtos/index.js
+++ src/paginasRestaurante/produtos/index.js
@@ -1,7 +1,34 @@
 import './styles.css';
+import '../../styles/global.css';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../routes';
+import illustrationTop from '../../assets/illustration-top.svg';
 
 export default function produtos() {
+  const { deslogar } = useContext(AuthContext);
+
   return (
-    <h1>Olar</h1>
+    <div className="bodyProdutos">
+      <div className="conteinerTopo contentCenter itemsCenter">
+        <div className="flexRow contentBetween itemsCenter">
+          <h1 className="nomeRestaurante">Pizza Pizzaria & Delivery</h1>
+          <Link className="logout" to="/login" onClick={deslogar}>Logout</Link>
+        </div>
+      </div>
+      <img className="vetorProdutos" src={illustrationTop} alt="vetor" />
+      <div className="avatarRestaurante">
+        .
+      </div>
+
+      <div className="conteudoProdutos flexColunm contentCenter itemsCenter">
+        <span>
+          Você ainda não tem nenhum produto no seu cardápio.
+          <br />
+          Gostaria de adicionar um novo produto?
+        </span>
+        <button className="btLaranja mt2rem" type="submit">Adicionar produtos</button>
+      </div>
+    </div>
   );
 }
