74c7075545440108ee0b20f2372fb2e811b010bd
Yuendel Farias <yuendel.fb@hotmail.com>
2021-07-30 19:42:27 -0300

    Context e integração no Login

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,32 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Switch, Redirect
+} from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import useAuth from './hooks/useAuth';
 
 import Login from './paginasRestaurante/login';
 import Cadastrar from './paginasRestaurante/cadastrar';
 
+function RotasProtegidas(props) {
+  const { token } = useAuth();
+
+  return (
+    <Route
+      render={() => (token ? props.children : <Redirect to="/" />)}
+    />
+  );
+}
 export default function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/login" component={Login} />
-        <Route path="/cadastrar" component={Cadastrar} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/cadastrar" component={Cadastrar} />
+          <RotasProtegidas />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
