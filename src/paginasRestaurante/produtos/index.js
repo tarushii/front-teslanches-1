1896c63cb83df59249727e75682ec432b4c29285
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-31 13:43:12 -0300

    conteiner mapProdutos+ listaTeste

--- src/paginasRestaurante/produtos/index.js
+++ src/paginasRestaurante/produtos/index.js
@@ -1,11 +1,49 @@
 import './styles.css';
 import '../../styles/global.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import illustrationTop from '../../assets/illustration-top.svg';
 import CustomizedDialogs from '../../componentes/customDialog';
 import ProdutosNovo from '../produtosNovo';
+import CustomCard from '../../componentes/customCard';
+import imgPizza from '../../assets/pizza.png';
 
+const testeProdutoLista = [
+  {
+    nome: 'teste',
+    descricao: 'blablabla',
+    valor: 1234,
+    imagem: imgPizza
+  },
+  {
+    nome: 'teste1',
+    descricao: 'blablabla',
+    valor: 1234,
+    imagem: imgPizza
+  },
+  {
+    nome: 'teste2',
+    descricao: 'blablabla',
+    valor: 1234,
+    imagem: imgPizza
+  },
+  {
+    nome: 'teste3',
+    descricao: 'blablabla',
+    valor: 1234,
+    imagem: imgPizza
+  },
+];
 export default function produtos() {
+  const [cardapio, setCardapio] = useState([]);
+  useEffect(() => {
+    // async function carregarCardapio() {
+    //   const resposta = await fetch(testeProdutoLista);
+    //   const cardapioRetornado = await resposta.json();
+    // }
+    setCardapio(testeProdutoLista);
+  }, []);
+
   return (
     <div className="bodyProdutos">
       <div className="conteinerTopo contentCenter itemsCenter">
@@ -19,14 +57,32 @@ export default function produtos() {
         .
       </div>
 
-      <div className="conteudoProdutos flexColunm contentCenter itemsCenter">
+      <div className={`${cardapio.length === 0 ? 'none' : 'contemProdutos'} flexColunm contentCenter itemsCenter mt2rem`}>
+        <div className="contemBotao flexRow">
+          <CustomizedDialogs
+            btAbrirMensagem={<> Adicionar produto ao cardápio</>}
+            btMensagem={<>Adicionar produto novo</>}
+            conteudo={<ProdutosNovo />}
+          />
+        </div>
+
+        <div className="conteinerCardapio flexRow gap2rem">
+          { cardapio.map((produto) => (
+            <div className="cardProduto" item key={produto.id}>
+              <CustomCard {...produto} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={`${cardapio.length === 0 ? 'addProdutos' : 'none'} flexColunm contentCenter itemsCenter`}>
         <span>
           Você ainda não tem nenhum produto no seu cardápio.
           <br />
           Gostaria de adicionar um novo produto?
         </span>
         <CustomizedDialogs
-          btAbrirMensagem={<> Adicionar produtos</>}
+          btAbrirMensagem={<> Adicionar produto ao cardápio</>}
           btMensagem={<>Adicionar produto novo</>}
           conteudo={<ProdutosNovo />}
         />
