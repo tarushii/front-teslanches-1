87add77765297232b04a6a4ad5c8467e19c981d6
Uthan Hercules <uthan.hercules12@gmail.com>
2021-08-04 17:00:22 -0300

    Etapa 1 de cadastro finalizado com validações

--- src/paginasRestaurante/cadastrar/index.js
+++ src/paginasRestaurante/cadastrar/index.js
@@ -1,146 +1,177 @@
+import { React, useState } from 'react';
 import './styles.css';
 import '../../styles/global.css';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
-import illustrationCenter from '../../assets/illustration-center.svg';
-import CustomStepper from '../../componentes/customStepper';
-import InputPassword from '../../componentes/inputPassword';
-import InputSelect from '../../componentes/customCategoria';
 
-const data = {
-  nome: null,
-  email: null,
-  senha: null,
-  restaurante: {
-    nome: null,
-    descricao: null,
-    idCategoria: null,
-    taxaEntrega: null,
-    tempoEntregaEmMinutos: null,
-    valorMinimoPedido: null
-  }
-};
-
-function getStepContent(stepIndex) {
-  const { register } = useForm();
-  const [nome, setNome] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [conferePassword, setConferePassword] = useState('');
-  const [nomeRestaurante, setNomeRestaurante] = useState('');
-  const [categoria, setCategoria] = useState('');
-  const [descricao, setDescricao] = useState('');
-  const [taxa, setTaxa] = useState('');
-  const [tempo, setTempo] = useState('');
-  const [valorMinimo, setValorMinimo] = useState('');
-
-  data.nome = nome;
-  data.senha = password;
-  data.email = email;
-  data.restaurante.nome = nomeRestaurante;
-  data.restaurante.descricao = descricao;
-  data.restaurante.idCategoria = categoria;
-  data.restaurante.taxaEntrega = taxa;
-  data.restaurante.tempoEntregaEmMinutos = tempo;
-  data.restaurante.valorMinimoPedido = valorMinimo;
-
-  // Sinto que se algum outro engenheiro ver essa "solução"
-  // eu nunca mais terei permissão para codificar na vida.
-  // TODO - Isso aqui tudo de novo.
-
-  switch (stepIndex) {
-    case 0:
-      return (
-        <div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="nomeUsuarioRest">Nome de usuário</label>
-            <input id="nomeUsuarioRest" type="text" onChange={(e) => setNome(e.target.value)} required />
-          </div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="emailRest">E-mail</label>
-            <input id="emailRest" type="email" onChange={(e) => setEmail(e.target.value)} required />
-          </div>
-          <InputPassword
-            id="senha"
-            label="Senha"
-            register={() => register('senha', { required: true, minLength: 8 })}
-            value={password}
-            setValue={setPassword}
-          />
-          <InputPassword
-            id="senhaConfere"
-            label="Repita a senha"
-            register={() => register('senha', { required: true, minLength: 8 })}
-            value={conferePassword}
-            setValue={setConferePassword}
-          />
-        </div>
-      );
-    case 1:
-      return (
-        <div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="nomeRestaurante">Nome do restaurante</label>
-            <input id="nomeRestaurante" type="text" onChange={(e) => setNomeRestaurante(e.target.value)} required />
-          </div>
-
-          <InputSelect setCategoria={setCategoria} />
-
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="descricao">Descrição</label>
-            <textarea id="descricao" onChange={(e) => setDescricao(e.target.value)} required />
-            <span className="mr06rem">Máx.: 50 caracteres</span>
-          </div>
-        </div>
-      );
-    case 2:
-      return (
-        <div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="taxaEntrega">Taxa de entrega</label>
-            <input id="taxaEntrega" type="number" onChange={(e) => setTaxa(e.target.value)} required />
-          </div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="tempoEntrega">Tempo estimado de entrega</label>
-            <input id="tempoEntrega" type="time" onChange={(e) => setTempo(e.target.value)} required />
-          </div>
-          <div className="flexColunm mb1rem posRelative">
-            <label htmlFor="valorPedido">Valor mínimo do pedido</label>
-            <input id="valorPedido" type="money" onChange={(e) => setValorMinimo(e.target.value)} required />
-          </div>
-        </div>
-      );
-    default:
-      return 'Concluído!';
-  }
-}
+const stepper1 = document.querySelector('.item-1');
+const stepper2 = document.querySelector('.item-2');
+const stepper3 = document.querySelector('.item-3');
 
 export default function Cadastrar() {
-  return (
-    <div className="bodyCadastrar">
+  const [etapa, setEtapa] = useState(1);
+  const [dados, setDados] = useState({
 
-      <div className="conteinerFormCadastro">
-        <form className="formCadastro ">
-          <div className="espacoTopo" />
-          <div className="flexRow">
-            <div />
-            <div className="ml2rem" />
-            <CustomStepper
-              title={(<h1>Cadastro</h1>)}
-              getStepContent={getStepContent}
-              data={data}
-            />
-          </div>
-          <div className=" flexColunm mb1rem">
+  });
+
+  const validar = function validaDadosDoStepper(etapa) {
+    const validacao = {
+      mensagem: 'Nenhum erro encontrado.',
+      valido: true
+    };
+
+    if (etapa === 1) {
+      const dados = {
+        Nome: document.querySelector('.nome-usuario').value,
+        Email: document.querySelector('.email-usuario').value,
+        Senha: document.querySelector('.senha-usuario').value,
+        Confirma: document.querySelector('.confirma-senha-usuario').value
+      };
+
+      if (!dados.Nome || dados.Nome.trim() === '') {
+        validacao.mensagem = 'NOME é um campo obrigatório.';
+        validacao.valido = false;
+      } else if (!dados.Email || dados.Email.trim() === '' || !dados.Email.includes('@') || !dados.Email.includes('.com')) {
+        validacao.mensagem = 'E-MAIL é um campo obrigatório.';
+        validacao.valido = false;
+      } else if (!dados.Senha || dados.Senha.trim() === '') {
+        validacao.mensagem = 'SENHA é um campo obrigatório.';
+        validacao.valido = false;
+      } else if (dados.Senha.length < 8) {
+        validacao.mensagem = 'A senha deve conter, ao menos, 8 caracteres.';
+        validacao.valido = false;
+      } else if (dados.Senha !== dados.Confirma) {
+        validacao.mensagem = 'As senhas devem ser iguais.';
+        validacao.valido = false;
+      }
+
+      setDados({
+        nome: dados.Nome,
+        email: dados.Email,
+        senha: dados.Senha
+      });
+
+      console.log(dados);
+      return (validacao);
+    }
+    if (etapa === 2) {
+      return (validacao);
+    }
+    if (etapa === 3) {
+      return (validacao);
+    }
+    if (etapa === 4) {
+      return (validacao);
+    }
+
+    return (validacao);
+  };
+
+  const recuar = function recuaStepper() {
+    if (etapa === 1) {
+      return;
+    }
+    setEtapa(etapa - 1);
+  };
+
+  const avancar = function avancaStepper() {
+    const dadosValidos = validar(etapa);
+    const mensagemErro = document.querySelector('.erros');
+
+    if (dadosValidos.valido) {
+      mensagemErro.innerHTML = '';
+
+      setEtapa(etapa + 1);
+    } else {
+      mensagemErro.innerHTML = dadosValidos.mensagem;
+    }
+  };
+
+  const mostrarSenhas = function revelaSenhasParaUsuario() {
+    const senha = document.querySelector('.senha-usuario');
+    const confirmaSenha = document.querySelector('.confirma-senha-usuario');
+
+    if (senha.type === 'password') {
+      senha.type = 'text';
+      confirmaSenha.type = 'text';
+    } else {
+      senha.type = 'password';
+      confirmaSenha.type = 'password';
+    }
+  };
+
+  const formulario = function retornaDadosDosFormularios() {
+    switch (etapa) {
+      default:
+        return (
+          <>
+            <h1>Quebrou</h1>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <p>Nome de Usuário</p>
+            <input type="text" className="nome-usuario" />
+            <br />
+            <p>E-mail</p>
+            <input type="email" className="email-usuario" />
+            <br />
             <div>
-              <span className="mr06rem">Já tem uma conta?</span>
-              <Link to="/login">Login</Link>
+              <p>Senha</p>
+              <button type="button" onClick={() => mostrarSenhas()}>Mostrar senhas</button>
             </div>
-          </div>
-        </form>
-        <img className="vetorCadastro" src={illustrationCenter} alt="vetor" />
-      </div>
-    </div>
+            <input type="password" className="senha-usuario" />
+            <br />
+            <p>Repetir Senha</p>
+            <input type="password" className="confirma-senha-usuario" />
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <h1>Etapa 2</h1>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <h1>Etapa 3</h1>
+          </>
+        );
+      case 4:
+        return (
+          <>
+            <h1>Finalizado</h1>
+          </>
+        );
+    }
+  };
+
+  return (
+    <article className="telaRegistro">
+      <section className="telaRegistro__formulario">
+        <header>
+          <h1>Cadastro</h1>
+          <section className="formulario__stepper">
+            <div className="stepper--atual">
+              <p className="stepper__item item-1">1</p>
+            </div>
+            <div className="stepper--incompleto">
+              <p className="stepper__item item-2">2</p>
+            </div>
+            <div className="stepper--incompleto">
+              <p className="stepper__item item-3">3</p>
+            </div>
+          </section>
+        </header>
+        <main>
+          {formulario()}
+        </main>
+        <p className="erros"> </p>
+        <footer>
+          <button className="anterior-form" type="button" onClick={() => recuar()}>Anterior</button>
+          <button className="proximo-form" type="button" onClick={() => avancar()}>Próximo</button>
+        </footer>
+      </section>
+    </article>
   );
 }
