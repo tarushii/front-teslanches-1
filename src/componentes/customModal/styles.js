39c9ef1865529370d08e43a9e3652d74d1736049
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-09 22:16:42 -0300

    componente modal delete

---
+++ src/componentes/customModal/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+// Tive ajuda nesses estilos
+
+export default makeStyles((theme) => ({
+
+  botoes: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: 10,
+    marginTop: 15,
+    padding: 15,
+
+  },
+  MuiDialogPaper: {
+    borderRadius: 24,
+  }
+}));
