9bf181acb3b26e4bffbef2a0aaaef3228795b796
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-13 16:43:24 -0300

    switch +styles

--- src/paginasRestaurante/produtos/index.js
+++ src/paginasRestaurante/produtos/index.js
@@ -11,7 +11,7 @@ import ProdutosNovo from '../produtosNovo';
 import UsuarioEditar from '../usuarioEditar/index';
 import CustomCard from '../../componentes/customCard';
 import useAuth from '../../hooks/useAuth';
-import { get, del } from '../../services/apiClient';
+import { get } from '../../services/apiClient';
 
 import Diversos from '../../assets/bg-Diversos.png';
 import Pizzaria from '../../assets/bg-Pizzaria.png';
@@ -26,6 +26,7 @@ import Brasileira from '../../assets/bg-Brasileira.png';
 import Lanches from '../../assets/bg-Lanches.png';
 import CustomModal from '../../componentes/customModal';
 import ProdutosEditar from '../produtosEditar';
+import avatarPadrao from '../../assets/avatar-padrao.png';
 
 export default function produtos() {
   const { user, token, deslogar } = useAuth();
@@ -57,12 +58,12 @@ export default function produtos() {
         if (!ok) {
           return toast.error(`erro${dados}`);
         }
-        toast.error(dados);
+        toast.success(dados);
+
         return setUsuario(dados);
       } catch (error) {
-        toast.error(error.message);
+        return toast.error(error.message);
       }
-      return toast.error('Usuario');
     };
 
     buscarUsuario();
@@ -70,34 +71,35 @@ export default function produtos() {
   }, [token, f5]);
 
   const categoriaStyle = () => {
-    const categoria = user.Categoria;
+    const categoria = usuario.categoria_id;
     switch (categoria) {
       default:
         return { backgroundImage: `url(${Pizzaria})` };
-      case 'Diversos':
+      case 1:
         return { backgroundImage: `url(${Diversos})` };
-      case 'Lanches':
+      case 2:
         return { backgroundImage: `url(${Lanches})` };
-      case 'Carnes':
+      case 3:
         return { backgroundImage: `url(${Carnes})` };
-      case 'Massas':
+      case 4:
         return { backgroundImage: `url(${Massas})` };
-      case 'Pizzas':
+      case 5:
         return { backgroundImage: `url(${Pizzaria})` };
-      case 'Japonesa':
+      case 6:
         return { backgroundImage: `url(${Japonesa})` };
-      case 'Chinesa':
+      case 7:
         return { backgroundImage: `url(${Chinesa})` };
-      case 'Mexicano':
+      case 8:
         return { backgroundImage: `url(${Mexicano})` };
-      case 'Brasileira':
+      case 9:
         return { backgroundImage: `url(${Brasileira})` };
-      case 'Italiana':
+      case 10:
         return { backgroundImage: `url(${Italiana})` };
-      case 'Árabe':
+      case 11:
         return { backgroundImage: `url(${Arabe})` };
     }
   };
+
   return (
     <div className="bodyProdutos">
       <div style={categoriaStyle()} className="conteinerTopo contentCenter itemsCenter">
@@ -107,13 +109,9 @@ export default function produtos() {
         </div>
       </div>
       <img className="vetorProdutos" src={illustrationTop} alt="vetor" />
-
-      <img src={usuario.imagem_restaurante} alt="avatarRestaurante" className="avatarRestaurante" />
+      <img src={usuario.imagem_restaurante ? usuario.imagem_restaurante : avatarPadrao} alt="avatarRestaurante" className="avatarRestaurante" />
       <div className="avatarRestaurante">
-        <CustomizedDialogs
-          btClassName="btEditarUsuario"
-          conteudo={<UsuarioEditar {...usuario} recarregarPag={() => setF5(true)} />}
-        />
+        <UsuarioEditar {...usuario} recarregarPag={() => setF5(true)} />
       </div>
 
       <div className={`${prod.length === 0 ? 'none' : 'contemProdutos'} flexColunm contentCenter itemsCenter mt2rem`}>
@@ -123,7 +121,6 @@ export default function produtos() {
 
         <div className="conteinerCardapio flexRow gap2rem">
           { prod.map((produto) => (
-
             <div className="flip-card" item key={produto.id}>
               <div className="flip-card-inner">
                 <div className="flip-card-front">
@@ -133,15 +130,12 @@ export default function produtos() {
                   />
                 </div>
                 <div className="flip-card-back">
-
                   <CustomModal {...produto} recarregarPag={() => setF5(true)} />
                   { ' ' }
                   <ProdutosEditar {...produto} recarregarPag={() => setF5(true)} />
-
                 </div>
               </div>
             </div>
-
           ))}
         </div>
       </div>
